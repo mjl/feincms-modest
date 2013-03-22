bc624c798c99b83dde1baf3c2954b0bf2b4147fc
Martin J. Laubach <mjl@laubach.at>
2013-03-22 20:14:06 +0100

    Tweak setup.py a bit

--- setup.py
+++ setup.py
@@ -3,17 +3,19 @@ from setuptools import setup
 from gallery import __version__
 
 setup(
-    name             = "feincms-gallery",
-#    version          = __version__,
+    name             = "feincms-media-gallery",
+    version          = __version__,
     author           = "Martin J. Laubach",
-    author_email     = "pypi+feincms-gallery@laubach.at",
+    author_email     = "pypi+feincms-media-gallery@laubach.at",
     description      = ("Gallery content type for FeinCMS"),
     license          = "BSD",
-#    keywords         = "feincms content type gallery",
-    url              = "http://github.com/mjl/feincms-gallery",
-#    py_modules       = ['ecglist'],
+    keywords         = "feincms content type gallery",
+#    url              = "http://github.com/mjl/feincms-media-gallery",
+    packages         = ['gallery'],
     long_description = open('README.rst').read(),
     test_suite       = "tests",
+    zip_safe         = False,
+    package_data     = {'': ['gallery/templates']},
     classifiers      = [
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
